962c12cd4fef63f19c8f4d10d7f39a9edeab1ab8
Adam <xadamxk@gmail.com>
2021-08-07 16:57:29 -0500

    Update css selectors to be unique

--- node_helper.js
+++ node_helper.js
@@ -11,25 +11,29 @@ var defaultClient = LolesportsApi.ApiClient.instance;
 var api = new LolesportsApi.LeaguesApi();
 
 module.exports = NodeHelper.create({
-	// Override socketNotificationReceived method.
-	socketNotificationReceived: function (notification, payload) {
-		let self = this;
-		if (notification === "MMM-LOLESPORTS-STANDINGS-GET-STANDINGS") {
-			defaultClient.basePath = payload["basePath"];
-			defaultClient.authentications["apiKeyAuth"]["apiKey"] = payload["apiKey"];
+  // Override socketNotificationReceived method.
+  socketNotificationReceived: function (notification, payload) {
+    let self = this;
+    if (notification === "MMM-LOLESPORTS-STANDINGS-GET-STANDINGS") {
+      defaultClient.basePath = payload["basePath"];
+      defaultClient.authentications["apiKeyAuth"]["apiKey"] = payload["apiKey"];
 
-			api.getStandings(LolesportsApi.Locale.enUS, { tournamentId: payload["tournamentIds"] }, function (error, data, response) {
-				if (error) {
-					console.error(error);
-				} else {
-					// console.log("API called successfully. Returned data: " + JSON.stringify(data));
-					self.sendNotificationTest(data);
-				}
-			});
-		}
-	},
-	// Example function send notification test
-	sendNotificationTest: function (payload) {
-		this.sendSocketNotification("MMM-LOLESPORTS-STANDINGS-STANDINGS", payload);
-	}
+      api.getStandings(
+        LolesportsApi.Locale.enUS,
+        { tournamentId: payload["tournamentIds"] },
+        function (error, data, response) {
+          if (error) {
+            console.error(error);
+          } else {
+            // console.log("API called successfully. Returned data: " + JSON.stringify(data));
+            self.sendNotificationTest(data);
+          }
+        }
+      );
+    }
+  },
+  // Example function send notification test
+  sendNotificationTest: function (payload) {
+    this.sendSocketNotification("MMM-LOLESPORTS-STANDINGS-STANDINGS", payload);
+  }
 });
